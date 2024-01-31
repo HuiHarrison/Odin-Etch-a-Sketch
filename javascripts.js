63e4fffc59c7ac70371b93f18489e8c49ba95742
Harrison <109526528+HuiHarrison@users.noreply.github.com>
2024-01-31 15:48:13 +0800

    Add Hover to Box

--- javascripts.js
+++ javascripts.js
@@ -12,6 +12,10 @@ for(let y=0; y<16;y++){
     for (let x=0; x<16; x++) {
         const box = document.createElement("div");
         box.setAttribute("class", "box");
+        // Change colour if hover
+        box.addEventListener("mouseover", () => {box.setAttribute("style", "background-color: #1d3557;")}, false);
+        box.addEventListener("mouseout", () => {box.setAttribute("style", "background-color: #1d3557;")}, false);
         row.append(box);
     }
 }
+
